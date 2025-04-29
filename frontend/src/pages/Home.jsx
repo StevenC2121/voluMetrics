1996e6ee9519bf6e609f63fd9bf866630af32df8
StevenC2121 <stevencabral2121@gmail.com>
2025-04-29 10:08:21 -0400

    Building the home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,96 @@
+import { useState, useEffect } from "react";
+import api from "../api";
+
 function Home() {
-    return <div>Home</div>
+  const [exercises, setExercises] = useState([]);
+  const [weight, setWeight] = useState(null);
+  const [reps, setReps] = useState(null);
+  const [name, setName] = useState("");
+
+  useEffect(() => {
+    getExercises();
+  }, []);
+
+  const getExercises = () => {
+    api
+      .get("/api/exercises/")
+      .then((res) => res.data)
+      .then((data) => {
+        setExercises(data);
+        console.log(data);
+      })
+      .catch((err) => alert(err));
+  };
+
+  const deleteExercise = (id) => {
+    api
+      .delete(`/api/exercises/delete/${id}/`)
+      .then((res) => {
+        if (res.status === 204) alert("Exercise deleted!");
+        else alert("Failed to delete exercise.");
+        getExercises();
+      })
+      .catch((error) => alert(error));
+  };
+
+  const createExercise = (e) => {
+    e.preventDefault();
+    api
+      .post("/api/exercises/", { weight, reps, name })
+      .then((res) => {
+        if (res.status === 201) alert("Exercise created!");
+        else alert("Failed to create exercise.");
+        getExercises();
+      })
+      .catch((err) => alert(err));
+  };
+  return (
+    <div>
+      <div>
+        <h2>Exercises</h2>
+      </div>
+      <h2>Log a Lift</h2>
+      <form onSubmit={createExercise}>
+        <label htmlFor="name">Exercise Name:</label>
+        <br />
+        <input
+          type="text"
+          id="name"
+          name="name"
+          required
+          onChange={(e) => setName(e.target.value)}
+          value={name}
+        />
+
+        <label htmlFor="weight">Weight (lbs):</label>
+        <br />
+        <input
+          type="number"
+          id="weight"
+          name="weight"
+          step="0.1"
+          onChange={(e) => setWeight(parseFloat(e.target.value))}
+          value={weight ?? ""}
+          required
+        />
+
+        <label htmlFor="reps">Reps:</label>
+        <br />
+        <input
+          type="number"
+          id="reps"
+          name="reps"
+          min="1"
+          step="1"
+          onChange={(e) => setReps(parseInt(e.target.value))}
+          value={reps ?? ""}
+          required
+        />
+        <br />
+        <input type="submit" value="Submit"></input>
+      </form>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
