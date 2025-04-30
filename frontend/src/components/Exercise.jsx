fed822c9e4c5de465be7e8ad04a87c76d71835e4
StevenC2121 <stevencabral2121@gmail.com>
2025-04-30 12:05:59 -0400

    Updated delete route and component styling

--- frontend/src/components/Exercise.jsx
+++ frontend/src/components/Exercise.jsx
@@ -1,16 +1,17 @@
 import React from "react";
+import "../styles/Exercise.css"
 
 function Exercise({ exercise, onDelete }) {
-  const formattedDate = new Date(exercise.created_at).toLocaleDateString(
+  const formattedDate = new Date(exercise.performed_at).toLocaleDateString(
     "en-us"
   );
 
   return (
     <div className="exercise-container">
       <p className="exercise-name">{exercise.name}</p>
-      <p className="exercise-weight">{exercise.weight}</p>
-      <p className="exercise-reps">{exercise.reps}</p>
-      <p className="exercise-date">{formattedDate}</p>
+      <p className="exercise-weight">Weight: {exercise.weight}</p>
+      <p className="exercise-reps">Reps: {exercise.reps}</p>
+      <p className="exercise-date">Date: {formattedDate}</p>
       <button className="delete-button" onClick={() => onDelete(exercise.id)}>
         Delete
       </button>
